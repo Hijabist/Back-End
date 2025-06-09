7f4ffae4908e71dd7be4f7c4627fb24ed0b1198b
Haikal <abizarhaikal@gmail.com>
2025-06-09 21:56:01 +0700

    fix my deploy

--- api/index.js
+++ api/index.js
@@ -1,21 +1,23 @@
 const express = require("express");
-const serverless = require("serverless-http");
 const path = require("path");
 
 const authRoutes = require("../src/routes/auth/authRoutes.js");
 const faceShapeRoutes = require("../src/routes/predict/face-shape-routes.js");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Static modeltfjs (misal: modeltfjs ada di src/modeltfjs)
+// Static modeltfjs
 app.use("/modeltfjs", express.static(path.join(__dirname, "../src/modeltfjs")));
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/predict", faceShapeRoutes);
 
-// Export sebagai handler
-module.exports = serverless(app);
+// Jalankan server Express biasa
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
